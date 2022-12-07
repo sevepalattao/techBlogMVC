0f55ffa0fc4e6667c665e27a69d592a2544030bd
BigSev15 <palat008@umn.edu>
2022-12-07 15:15:51 -0600

    got comments to display when a single post is clicked on and setup single post route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,6 +24,37 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('onePost', {
+            post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
       res.redirect('/');
