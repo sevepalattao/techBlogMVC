f91bd4c187ecfff79f44656b54fb3b78236f491c
BigSev15 <palat008@umn.edu>
2022-12-08 13:58:58 -0600

    got comments to work after some great help from askbcs

--- public/js/Comment.js
+++ public/js/Comment.js
@@ -1,20 +1,20 @@
 const commentFormHandler = async (event) => {
     event.preventDefault();
 
-    const comment_text = document.querySelector('#comment-body').value.trim();
+    const text = document.querySelector('#comment-body').value.trim();
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
 
-    if (comment_text) {
+    if (text) {
         const response = await fetch('/api/comments', {
             method: 'POST',
-            body: JSON.stringify({ comment_text, post_id }),
+            body: JSON.stringify({ text, post_id }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace(`/post/${post_id}`);
         } else {
             alert('Failed to add comment.');
         }
